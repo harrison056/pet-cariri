b10aed7c73ce91cb9b3b06f303c253a3920ddca1
harrison056 <costa.silva.cs.cs@gmail.com>
2020-03-29 20:11:18 -0300

    Criação da tabela Admin

--- config/auth.php
+++ config/auth.php
@@ -41,6 +41,11 @@ return [
             'provider' => 'users',
         ],
 
+        'admin' => [
+            'driver' => 'session',
+            'provider' => 'admin',
+        ],
+
         'api' => [
             'driver' => 'token',
             'provider' => 'users',
@@ -71,6 +76,11 @@ return [
             'model' => App\User::class,
         ],
 
+
+        'admin' => [
+            'driver' => 'eloquent',
+            'model' => App\Admin::class,
+        ],
         // 'users' => [
         //     'driver' => 'database',
         //     'table' => 'users',
